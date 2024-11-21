cee30599876d281e87e9c688295bd0524b71dfef
Jojo Mitchell <jd72530@gmail.com>
2024-11-20 20:35:54 -0800

    create connect-logo

---
+++ src/components/ui/connect-logo.tsx
@@ -0,0 +1,21 @@
+export default function ConnectLogo() {
+  const logoSize = 52;
+  return (
+    <div
+      className="logo-container"
+      style={{
+        width: logoSize,
+        height: logoSize,
+      }}
+    >
+        <img 
+          src="/logo.svg" 
+          alt="Connect Four Logo"
+          className="logo"
+          width={logoSize}
+          height={logoSize}
+        />
+
+    </div>
+  );
+}
